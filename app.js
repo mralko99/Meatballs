bb8f4f7eac52d313f1abd49df507341b25335812
sigsegv1 <64968167+sigsegv1@users.noreply.github.com>
2020-07-11 12:56:41 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,78 @@
+const express = require('express'); //returns a function
+const app = express(); //returns an object of type Express
+
+app.use(express.json()); //(per il req.body.name)
+
+// PORT (ENVIROMENT VARIABLE)  //a variable that is part of the enviroment in which teh process runs, its value is set outside this application
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
+//scrivendo su terminale "$env:PORT = 5000" la macchina si metterà ad ascoltare sulla porta 5000
+
+app.get('/api/composition', (req, res) => {
+
+  var unirest = require("unirest");
+  var request = unirest("GET", "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/quickAnswer");
+  console.log('ok1');
+  console.log(req.query.id);
+  console.log('ok2');
+  var info = req.query.id;  // example of terminal request: curl localhost:5000/api/composition?id=protein%20pepperoni
+	                          // => answer (on terminal): In 1 serving pepperoni, there are 6.35 g of Protein. This amount covers 13% of your daily needs of Protein.
+
+  request.query({
+    "q": info
+  });
+
+
+  request.headers({
+    "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+    "x-rapidapi-key": "8b7bf76a17mshbf959543e115761p120952jsn499463a234ee",
+    "useQueryString": true
+  });
+
+
+
+  request.end(function (result) {
+  	if (result.error) throw new Error(result.error);
+    res.send(result.body.answer);
+  });
+
+
+
+});
+
+
+
+
+
+app.get('/api/mealplans', (req, res) => {
+	var unirest = require("unirest");
+
+	var request = unirest("GET", "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate");
+
+	var calories = req.query.calories;  //example of terminal request: curl localhost:5000/api/mealplans?calories=4000
+
+
+	request.query({
+		"timeFrame": "day",
+		"targetCalories": calories,
+		"diet": "", //vegetarian"
+		"exclude": "shellfish%2C olives"
+	});
+
+	request.headers({
+		"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+		"x-rapidapi-key": "8b7bf76a17mshbf959543e115761p120952jsn499463a234ee",
+		"useQueryString": true
+	});
+
+
+	request.end(function (result) {
+		if (result.error) throw new Error(result.error);
+		var meals = result.body.meals;
+		//console.log(meals); ok, meals is a json var containing the array of all possible meals
+
+		// così funziona: res.send("The meal that you are searching for is: "+ result.body.meals[0].title + " \nHere are the nutrional values: \n" + "Calories = " + result.body.nutrients.calories + "\nProteins =  " + result.body.nutrients.protein +  "\nFat =  " + result.body.nutrients.fat + "\nCarbohydrates=  " + result.body.nutrients.carbohydrates       );
+		res.send("\n\n\nHere are your three meals: \n"+ result.body.meals[0].title + "\n" + result.body.meals[1].title + "\n" + result.body.meals[2].title + "\nHere are the nutrional values: \n" + "Calories = " + result.body.nutrients.calories + "\nProteins =  " + result.body.nutrients.protein +  "\nFat =  " + result.body.nutrients.fat + "\nCarbohydrates=  " + result.body.nutrients.carbohydrates       ); 
+		//console.log(result.body);
+	});
+});
