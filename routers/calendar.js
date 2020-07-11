513a0ed5b845b286141507c385447219fcae326b
Ale <lamin.1808791@studenti.uniroma1.it>
2020-07-11 16:49:48 +0200

    router commit

--- routers/calendar.js
+++ routers/calendar.js
@@ -1,36 +1,54 @@
 
 var request = require("request")
 
+var opener = require("opener")
+
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+
 auth_url = "https://accounts.google.com/o/oauth2/v2/auth"
 
 calendar_scope = "https://www.googleapis.com/auth/calendar"
 
 redirect_uri = "http://localhost:1234/test_auth"
 
-client_id = "82794785693-68v6o2ab2o9vfmstis28fqnq55jmmoud.apps.googleusercontent.com"
-
 token_request_url = "https://oauth2.googleapis.com/token"
 
-client_secret = "83g3NJuyobsIVx6XOTss154S"
 
-access_token = "ya29.a0AfH6SMAXXks6xShgdHCHOngofj4fAIQqkEUtvZJckdhd43UENPL0W54BdGLVWoi8yL22LqJWuYMcVQPKAvsKUwB7frAMrYZOmbPTAD5p_c6ay5Qd4EwjFvJZJ49N_fBLxs_rqc5XoAIwpyR3Z6N8LKHW3yab2lhDQr0"
+//api HTTP endpoints
+create_calendar_endpoint = "https://www.googleapis.com/calendar/v3/calendars"
+//
 
-opener = require("opener")
+
+client_id = process.env.GOOGLE_CLIENT_ID
+
+api_key = process.env.GOOGLE_API_KEY
+
+client_secret = process.env.GOOGLE_CLIENT_SECRET
 
 
+
+access_token = ""
+
+
+
+
+//open browser and visualizes authorization window
 function getAuthCode(){
   opener(auth_url+"?client_id="+client_id+"&redirect_uri="+redirect_uri+"&scope="+calendar_scope+"&response_type=code")
-
 }
 
 
+//get access token function
 function getAccessToken(code, redirect_uri){
   url = token_request_url
-  //headers = {"Content-Type":"application/x-www-form-urlencoded"}
+
   body = {
     "code":code,
     "client_id":client_id,
-    "client_secret":client_secret,
+    "client_secret":process.env.GOOGLE_CLIENT_SECRET,
     "redirect_uri":redirect_uri,
     "grant_type":"authorization_code"
   }
@@ -42,6 +60,7 @@ function getAccessToken(code, redirect_uri){
       console.error("token request failed");
       console.error("error: "+err.name+" message: "+err.message)
     }
+
     body_json = body
     access_token = body_json.access_token
     console.log("request_token_message_response "+JSON.stringify(response))
@@ -50,19 +69,16 @@ function getAccessToken(code, redirect_uri){
 
 }
 
-function log_token(access_token){
-    console.log(access_token)
-}
+
 
 function create_calendar(name){
-  url = "https://www.googleapis.com/calendar/v3/calendars"+"?key="+"AIzaSyAuyIrXHOgdaesCVGQC1nSsWicEQXg4zzU"
+  url = create_calendar_endpoint+"?key="+api_key
 
   body = {"summary":name}
   console.log(name)
   headers = {"Authorization": "Bearer "+access_token,
              "Accept": "application/json"
   }
-
   request.post({
     url:url,
     body:body,
@@ -70,10 +86,19 @@ function create_calendar(name){
     headers:headers}, function(err,response,body){
     console.log(body)
   })
+
+
+
+
+
+
+
+
+
+
 }
 module.exports = {
   getAuthCode,
   getAccessToken,
-  log_token,
   create_calendar,
 }
--- routers/calendar_tests.js
+++ routers/calendar_tests.js
@@ -12,7 +12,7 @@ redirect_uri = "http://localhost:1234/test_auth"
 
 client_id = "82794785693-68v6o2ab2o9vfmstis28fqnq55jmmoud.apps.googleusercontent.com"
 
-access_code = "" //"4/1wGfDb_kqj7K_jcHfMLz7hyA_jd2BwEZI6xPOSwvxKWItAO4_fzURtBZGf-3D7VND5Bojw0MIh4JWXyo6VL29ug"
+access_code = ""     //"4/1wGfDb_kqj7K_jcHfMLz7hyA_jd2BwEZI6xPOSwvxKWItAO4_fzURtBZGf-3D7VND5Bojw0MIh4JWXyo6VL29ug"
 
 
 
@@ -26,15 +26,22 @@ app.get("/test_calendar/get_code_opener", function(req,res){
 
 })
 
+
+//#################put in app.js!!!!
 app.get("/test_auth", function(req,res){
   access_code = req.query.code
   res.send("access code got!!!"+access_code)
 })
+//###############################
+
+
 
 app.get("/test_calendar/get_token", function(req,res){
   calendar.getAccessToken(access_code, "http://localhost:1234/test_auth")
-  res.send(calendar.log_token())
+  res.send("getting token....")
 })
+
+
 
 app.get("/test_calendar/create_calendar", function(req,res){
 
