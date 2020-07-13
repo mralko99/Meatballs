7a5b1ad0a90893b904997ae0097582040458640d
Gabriele De Santis <gabriele1.desantis@gmail.com>
2020-07-13 18:22:02 +0200

    MongoDB update

--- routers/mongoDB.js
+++ routers/mongoDB.js
@@ -1,23 +1,61 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const mongoose = require('mongoose');
 const dotenv = require("dotenv").config();
 
 //connecting to the database
 const URI = process.env.MONGODB_URI;
-const client = new MongoClient(URI);
+mongoose.connect(URI, { useNewUrlParser: true });
+const db = mongoose.connection;
 
-client.connect(function(err) {
-  if(err) throw err;
-  console.log("Connected successfully to server");
-});
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+var Schema = mongoose.Schema;
 
-function insertDB(req, meals){
-    const db = client.db(process.env.DB_NAME);
-    db.collection('meals').insertOne(req.body, function(err, res) {
-      if(err) throw err;
-      console.log("Successfully inserted into the database!");
-    });
+//Schema for a user
+var userSchema = new Schema({
+  username: String,
+  password: String
+})
+
+var User = mongoose.model('User', userSchema);
+
+//Method to save a user instance in the users collection.
+function createUser(username, password){
+  var userInstance = new User({
+    username: username,
+    password: password
+  })
+
+  userInstace.save(function(err) {
+    if (err) throw err;
+  })
+}
+
+//Method to find a user in the collection give the username.
+//Returns "" if the username is not in the collection, otherwise it returns the password associated.
+function findUser(username) {
+  User.findOne({ username: username }, { username: 1, password: 1 }, function(err, res) {
+    if (err) throw err;
+    else if (res == null) return "";
+    else return password;
+  })
+}
+
+/*
+function insertMeals(result){
+  const db = client.db(process.env.DB_NAME);
+  db.collection('meals').insertOne(result.body, function(err, res) {
+    if(err) throw err;
+    console.log("Successfully inserted the meals into the database!");
+  });
 };
 
-client.close();
-module.exports.insertDB = insertDB;
+function retrieveIdMeal(req, meals){
+  const db = client.db(process.env.DB_NAME);
+  var object = db.collection('meals').find().limit(1).sort({$natural:-1});
+  if(req == "colazione"){
+    object.find( {meals:0} );
+  }
+}
+
+module.exports.insertMeals = insertMeals;
+module.exports.retrieveIdMeal = retrieveIdMeal;
+*/
