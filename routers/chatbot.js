f1d05470c6632a5c19c3835c8c939b020f4eb531
Ale <lamin.1808791@studenti.uniroma1.it>
2020-07-13 12:53:17 +0200

    chatbot start

--- routers/chatbot.js
+++ routers/chatbot.js
@@ -9,13 +9,55 @@ const app = express ()
 
 enablews(app)
 
+const start_endpoint = "/chatbot"
+
 /*
 bot.start((ctx) => ctx.reply("Welcome!\nSend /start to start conversation"))
 bot.command('start', (ctx) => ctx.reply)
 */
 
-app.ws("/echo", (ws,req)=> {
+app.ws(start_endpoint, (ws,req)=> {
 
+  ws.send('insert username')
+
+  ws.on("message",username =>{
+    password = get_user(username).password
+    password = ""
+  })//.then()
+
+  msg=""
+
+
+
+       ws.on("message",msg =>{
+         if(password == ""){
+           console.log(msg)
+           if(msg == ""){
+             ws.send("l'account non esiste, inserisci una password per crearne uno")
+           }else{
+           //inserire funzione per salvare utente
+            registrato(ws)
+          }
+         }else{
+
+            if (password!=msg) {
+                ws.send("insert password")
+            }else {
+                ws.send("Password corretta")
+                registrato(ws)
+            }
+         }
+      })
+
+
+
+  /*
+  .then(ws.on('message', msg => {
+    ws.send(msg+"kfojofk")
+  }))
+
+
+  /*
   ws.on('message',msg => {
     switch (msg) {
       case 'ciao':
@@ -35,6 +77,7 @@ app.ws("/echo", (ws,req)=> {
 
     }
   })
+  */
 
   ws.on('close',()=>{
     console.log("connection closed")
@@ -42,4 +85,15 @@ app.ws("/echo", (ws,req)=> {
   })
 })
 
+function registrato(ws){
+  ws.send("Benvenuto, utente registrato")
+}
+
+
+function get_user(user){
+  user_json = {
+    "password":"test"
+  }
+  return user_json
+}
 app.listen(5000)
