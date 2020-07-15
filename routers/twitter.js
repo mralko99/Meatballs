000d3925773d977a690f4f918d6979d18cfb622a
Alberto <coluzzi.181478@studenti.uniroma1.it>
2020-07-15 17:40:09 +0200

    Twitter Funzionante

---
+++ routers/twitter.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var inspect = require('util-inspect');
+var oauth = require('oauth');
+const dotenv = require("dotenv").config()
+
+var app = express();
+
+// Get your credentials here: https://dev.twitter.com/apps
+var _twitterConsumerKey = process.env.CONSUMER_KEY;
+var _twitterConsumerSecret = process.env.CONSUMER_SECRET;
+
+var consumer = new oauth.OAuth(
+    "https://twitter.com/oauth/request_token", "https://twitter.com/oauth/access_token", 
+    _twitterConsumerKey, _twitterConsumerSecret, "1.0", "http://127.0.0.1:8080/sessions/callback", "HMAC-SHA1");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(session({ secret: "very secret", resave: false, saveUninitialized: true}));
+
+app.use(function(req, res, next) {
+  res.locals.session = req.session;
+  next();
+});
+
+app.get('/sessions/connect', function(req, res){
+  consumer.getOAuthRequestToken(function(error, oauthToken, oauthTokenSecret, results){
+    if (error) {
+      res.status(500).send("Error getting OAuth request token : " + inspect(error));
+    } else {
+      req.session.oauthRequestToken = oauthToken;
+      req.session.oauthRequestTokenSecret = oauthTokenSecret;
+      res.redirect("https://twitter.com/oauth/authorize?oauth_token="+req.session.oauthRequestToken);
+    }
+  });
+});
+
+app.get('/sessions/callback', function(req, res){
+  consumer.getOAuthAccessToken(req.session.oauthRequestToken, req.session.oauthRequestTokenSecret, req.query.oauth_verifier, function(error, oauthAccessToken, oauthAccessTokenSecret, results) {
+    if (error) {
+      res.send("Error getting OAuth access token : " + inspect(error) + "[" + oauthAccessToken + "]" + "[" + oauthAccessTokenSecret + "]" + "[" + inspect(results) + "]", 500);
+    } else {
+      req.session.oauthAccessToken = oauthAccessToken;
+      req.session.oauthAccessTokenSecret = oauthAccessTokenSecret;
+      console.log(oauthAccessToken)
+      console.log(oauthAccessTokenSecret)
+      res.send("Successful Authentication, you can close this window")
+    }
+  });
+});
+
+
+app.listen(8080, function() {
+  console.log('App runining on port 8080!');
+});
